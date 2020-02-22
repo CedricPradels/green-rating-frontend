caed2b28653ce309abec29ef58cbad0ade49ad47
CedricPradels <cedric.pradels@gmail.com>
2020-02-22 19:15:48 +0100

    Minimal project for deploy on netlify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// AXIOS
+import axios from "axios";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    const getAPI = async () => {
+      setIsLoading(true);
+      const response = await axios.get(
+        `${process.env.REACT_APP_STEINER_API}/test`
+      );
+      setValue(response.data.message);
+      setIsLoading(false);
+    };
+    getAPI();
+  }, []);
+
+  return <>{!isLoading && value}</>;
+};
 
 export default App;
